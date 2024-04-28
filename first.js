c124311ceaa2ac8e6eaf379ad6a758697a131d3f
Deepak Sati <deepaksati530@gmail.com>
2024-04-29 05:03:38 +0530

    updated code

--- first.js
+++ first.js
@@ -1,10 +1,31 @@
-let fname = 'deepaksatii'
+/*let fname = '   deepaksatii   ';
 
-console.log(fname)
+console.log(fname);
 
 let length = fname.length
 
-console.log(length)
+console.log(length);
 
-console.log(fname[2])
+console.log(fname[2]);
 
+console.log(fname.toUpperCase());
+console.log(fname.toLowerCase());
+
+let nname=fname.trim();
+
+console.log(nname);
+
+let sname=fname.slice(0,5);
+
+console.log(sname);
+
+*/
+
+var str = "50";
+var num = 50;
+
+var cstr = +str;
+var cnum = num + "";
+
+console.log(typeof cstr);
+console.log(typeof cnum);
